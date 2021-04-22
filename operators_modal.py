d99593fb3548fd7b3db6ebe9105149bc42ab01e7
Cody Winchester <codywinchart@gmail.com>
2021-04-22 12:22:22 -0500

    Update operators_modal.py

--- operators_modal.py
+++ operators_modal.py
@@ -87,194 +87,194 @@ class ABN_OT_normal_editor_modal(Operator):
             self.report({'WARNING'}, "Active object is not a mesh")
             return {'CANCELLED'}
 
+        if context.space_data.type != 'VIEW_3D':
+            self.report({'WARNING'}, "Active space must be a View3d")
+            return {'CANCELLED'}
+
         if context.active_object.mode != 'OBJECT':
             bpy.ops.object.mode_set(mode='OBJECT')
 
-        if context.space_data.type == 'VIEW_3D':
-            # INITIALIZE PROPERTIES
-            self._addon_prefs = bpy.context.preferences.addons[__package__.split('.')[
-                0]].preferences
-            self._display_prefs = self._addon_prefs.display
-            self._behavior_prefs = self._addon_prefs.behavior
-            self._keymap_sel_prefs = self._addon_prefs.keymap_sel
-            self._keymap_shortcut_prefs = self._addon_prefs.keymap_shortcut
-            self._keymap_tool_prefs = self._addon_prefs.keymap_tool
-            self._mouse_abs_loc = np.array([event.mouse_x, event.mouse_y, 0.0])
-            self._mouse_reg_loc = np.array(
-                [event.mouse_region_x, event.mouse_region_y, 0.0])
-            self._prev_mouse_loc = np.array(
-                [event.mouse_region_x, event.mouse_region_y, 0.0])
+        # INITIALIZE PROPERTIES
+        self._addon_prefs = bpy.context.preferences.addons[__package__.split('.')[
+            0]].preferences
+        self._display_prefs = self._addon_prefs.display
+        self._behavior_prefs = self._addon_prefs.behavior
+        self._keymap_sel_prefs = self._addon_prefs.keymap_sel
+        self._keymap_shortcut_prefs = self._addon_prefs.keymap_shortcut
+        self._keymap_tool_prefs = self._addon_prefs.keymap_tool
+        self._mouse_abs_loc = np.array([event.mouse_x, event.mouse_y, 0.0])
+        self._mouse_reg_loc = np.array(
+            [event.mouse_region_x, event.mouse_region_y, 0.0])
+        self._prev_mouse_loc = np.array(
+            [event.mouse_region_x, event.mouse_region_y, 0.0])
 
-            self._mouse_init = np.array([0.0, 0.0, 0.0])
-            self._active_point = None
-            self._active_face = None
+        self._mouse_init = np.array([0.0, 0.0, 0.0])
+        self._active_point = None
+        self._active_face = None
 
-            self._mode_cache = []
-            self._line_drawing_pos = []
+        self._mode_cache = []
+        self._line_drawing_pos = []
 
-            self._copy_normals = np.array([])
+        self._copy_normals = np.array([])
 
-            self.target_strength = 1.0
-            self._target_emp = None
-            self.point_align = False
+        self.target_strength = 1.0
+        self._target_emp = None
+        self.point_align = False
 
-            self.translate_mode = 0
-            self.translate_axis = 2
-            self.translate_draw_line = []
+        self.translate_mode = 0
+        self.translate_axis = 2
+        self.translate_draw_line = []
 
-            self._rot_increment_one = True
-            self._rot_increment_five = False
-            self._rot_increment_ten = False
-            self._rot_increment = 1
+        self._rot_increment_one = True
+        self._rot_increment_five = False
+        self._rot_increment_ten = False
+        self._rot_increment = 1
 
-            self._object_smooth = True
+        self._object_smooth = True
 
-            self._smooth_iterations = 5
-            self._smooth_strength = 0.25
+        self._smooth_iterations = 5
+        self._smooth_strength = 0.25
 
-            self._mirror_range = 0.1
+        self._mirror_range = 0.1
 
-            self._mirror_x = False
-            self._mirror_y = False
-            self._mirror_z = False
+        self._mirror_x = False
+        self._mirror_y = False
+        self._mirror_z = False
 
-            self._current_filter = ''
+        self._current_filter = ''
 
-            self._draw_area = context.area
-            self._modal_running = True
-            self.redraw = False
-            self.redraw_active = False
-            self.circle_radius = 50
+        self._draw_area = context.area
+        self._modal_running = True
+        self.redraw = False
+        self.redraw_active = False
+        self.circle_radius = 50
 
-            context.scene.abnormal_props.object = context.active_object.name
+        context.scene.abnormal_props.object = context.active_object.name
 
-            # VIEWPORT DISPLAY SETTINGS
-            self._x_ray_mode = False
-            self._use_gizmo = self._behavior_prefs.rotate_gizmo_use
-            self._gizmo_size = self._display_prefs.gizmo_size
-            self._normal_size = self._display_prefs.normal_size
-            self._line_brightness = self._display_prefs.line_brightness
-            self._point_size = self._display_prefs.point_size
-            self._loop_tri_size = self._display_prefs.loop_tri_size
-            self._selected_only = self._display_prefs.selected_only
-            self._selected_scale = self._display_prefs.selected_scale
-            self._individual_loops = self._behavior_prefs.individual_loops
-            if self._display_prefs.ui_scale == 0.0:
-                self._ui_scale = context.window.width/1920
-            else:
-                self._ui_scale = self._display_prefs.ui_scale
-            self.prev_view = context.region_data.view_matrix.copy()
-
-            # CACHE VIEWPORT SETTINGS
-            viewport_change_cache(self, context)
-
-            # NAVIGATION KEYS LIST
-            init_nav_list(self)
-
-            # MODES
-            self.rotating = False
-            self.sphereize_mode = False
-            self.point_mode = False
-            self.gizmo_click = False
-            self.waiting = False
-
-            self.box_selecting = False
-            self.lasso_selecting = False
-            self.circle_selecting = False
-            self.circle_resizing = False
-            self.circle_removing = False
-
-            self._current_tool = None
-            self.tool_mode = False
-
-            self.click_hold = False
-            self.selection_drawing = True
-            self.typing = False
-            self.bezier_changing = False
-            self.ui_hover = False
-
-            # UNDO STACK STORAGE
-            self._history_stack = []
-            self._history_select_stack = []
-            self._history_normal_stack = []
-            self._history_position = 0
-            self._history_select_position = 0
-            self._history_normal_position = 0
-            self._history_steps = 128
-
-            # INITIALIZE OBJECTS
-            self._objects_mod_status = []
-            self._objects_sk_vis = []
-            ob_bm, ob_kd, ob_bvh = ob_data_structures(
-                self, context.active_object)
-
-            # INITIALIZE OBJECT DATA LISTS
-            self._object = context.active_object
-            self._object_name = context.active_object.name
-            self._object_pointer = context.active_object.as_pointer()
-
-            self._object_bm = ob_bm
-            self._object_bvh = ob_bvh
-            self._object_kd = ob_kd
-
-            if self._object.data.use_auto_smooth == False:
-                self._object.data.use_auto_smooth = True
-                self._object.data.auto_smooth_angle = 180
-
-            # INITIALIZE BATCHES AND SHADERS
-            self.shader_2d = gpu.shader.from_builtin('2D_UNIFORM_COLOR')
-            self.shader_3d = gpu.shader.from_builtin('3D_UNIFORM_COLOR')
-
-            self._container = ABNContainer(
-                self._object.matrix_world.normalized())
-            self._container.set_scale_selection(self._selected_scale)
-            self._container.set_brightess(self._line_brightness)
-            self._container.set_normal_scale(self._normal_size)
-            self._container.set_point_size(self._point_size)
-            self._container.set_loop_scale(self._loop_tri_size)
-            self._container.set_draw_only_selected(self._selected_only)
-            self._container.set_draw_tris(self._individual_loops)
-
-            # INITIALIZE POINT DATA
-            cache_point_data(self)
-            self._orbit_ob = add_orbit_empty(self._object)
-            self._target_emp = add_target_empty(self._object)
-
-            update_filter_weights(self)
-
-            # INITIALIZE UI WINDOW
-            load_keymap(self)
-            init_ui_panels(self, rw, rh, self._ui_scale)
-
-            update_orbit_empty(self)
-
-            setup_tools(self)
-
-            # SETUP BATCHES
-            self._container.clear_batches()
-            refresh_batches(self, context)
-
-            # OPENGL DRAWING HANDLER
-            args = (self, context)
-            self._draw_handle_2d = bpy.types.SpaceView3D.draw_handler_add(
-                draw_callback_2d, args, "WINDOW", "POST_PIXEL")
-            self._draw_handle_3d = bpy.types.SpaceView3D.draw_handler_add(
-                draw_callback_3d, args, "WINDOW", "POST_VIEW")
-
-            dns = bpy.app.driver_namespace
-            dns["dh2d"] = self._draw_handle_2d
-            dns["dh3d"] = self._draw_handle_3d
-
-            add_to_undostack(self, 2)
-
-            self._window.check_in_window()
-
-            # SET MODAL
-            context.window_manager.modal_handler_add(self)
-            return {'RUNNING_MODAL'}
+        # VIEWPORT DISPLAY SETTINGS
+        self._x_ray_mode = False
+        self._use_gizmo = self._behavior_prefs.rotate_gizmo_use
+        self._gizmo_size = self._display_prefs.gizmo_size
+        self._normal_size = self._display_prefs.normal_size
+        self._line_brightness = self._display_prefs.line_brightness
+        self._point_size = self._display_prefs.point_size
+        self._loop_tri_size = self._display_prefs.loop_tri_size
+        self._selected_only = self._display_prefs.selected_only
+        self._selected_scale = self._display_prefs.selected_scale
+        self._individual_loops = self._behavior_prefs.individual_loops
+        if self._display_prefs.ui_scale == 0.0:
+            self._ui_scale = context.window.width/1920
         else:
-            self.report({'WARNING'}, "Active space must be a View3d")
-            return {'CANCELLED'}
+            self._ui_scale = self._display_prefs.ui_scale
+        self.prev_view = context.region_data.view_matrix.copy()
+
+        # CACHE VIEWPORT SETTINGS
+        viewport_change_cache(self, context)
+
+        # NAVIGATION KEYS LIST
+        init_nav_list(self)
+
+        # MODES
+        self.rotating = False
+        self.sphereize_mode = False
+        self.point_mode = False
+        self.gizmo_click = False
+        self.waiting = False
+
+        self.box_selecting = False
+        self.lasso_selecting = False
+        self.circle_selecting = False
+        self.circle_resizing = False
+        self.circle_removing = False
+
+        self._current_tool = None
+        self.tool_mode = False
+
+        self.click_hold = False
+        self.selection_drawing = True
+        self.typing = False
+        self.bezier_changing = False
+        self.ui_hover = False
+
+        # UNDO STACK STORAGE
+        self._history_stack = []
+        self._history_select_stack = []
+        self._history_normal_stack = []
+        self._history_position = 0
+        self._history_select_position = 0
+        self._history_normal_position = 0
+        self._history_steps = 128
+
+        # INITIALIZE OBJECTS
+        self._objects_mod_status = []
+        self._objects_sk_vis = []
+        ob_bm, ob_kd, ob_bvh = ob_data_structures(
+            self, context.active_object)
+
+        # INITIALIZE OBJECT DATA LISTS
+        self._object = context.active_object
+        self._object_name = context.active_object.name
+        self._object_pointer = context.active_object.as_pointer()
+
+        self._object_bm = ob_bm
+        self._object_bvh = ob_bvh
+        self._object_kd = ob_kd
+
+        if self._object.data.use_auto_smooth == False:
+            self._object.data.use_auto_smooth = True
+            self._object.data.auto_smooth_angle = 180
+
+        # INITIALIZE BATCHES AND SHADERS
+        self.shader_2d = gpu.shader.from_builtin('2D_UNIFORM_COLOR')
+        self.shader_3d = gpu.shader.from_builtin('3D_UNIFORM_COLOR')
+
+        self._container = ABNContainer(
+            self._object.matrix_world.normalized())
+        self._container.set_scale_selection(self._selected_scale)
+        self._container.set_brightess(self._line_brightness)
+        self._container.set_normal_scale(self._normal_size)
+        self._container.set_point_size(self._point_size)
+        self._container.set_loop_scale(self._loop_tri_size)
+        self._container.set_draw_only_selected(self._selected_only)
+        self._container.set_draw_tris(self._individual_loops)
+
+        # INITIALIZE POINT DATA
+        cache_point_data(self)
+        self._orbit_ob = add_orbit_empty(self._object)
+        self._target_emp = add_target_empty(self._object)
+
+        update_filter_weights(self)
+
+        # INITIALIZE UI WINDOW
+        load_keymap(self)
+        init_ui_panels(self, rw, rh, self._ui_scale)
+
+        update_orbit_empty(self)
+
+        setup_tools(self)
+
+        # SETUP BATCHES
+        self._container.clear_batches()
+        refresh_batches(self, context)
+
+        # OPENGL DRAWING HANDLER
+        args = (self, context)
+        self._draw_handle_2d = bpy.types.SpaceView3D.draw_handler_add(
+            draw_callback_2d, args, "WINDOW", "POST_PIXEL")
+        self._draw_handle_3d = bpy.types.SpaceView3D.draw_handler_add(
+            draw_callback_3d, args, "WINDOW", "POST_VIEW")
+
+        dns = bpy.app.driver_namespace
+        dns["dh2d"] = self._draw_handle_2d
+        dns["dh3d"] = self._draw_handle_3d
+
+        add_to_undostack(self, 2)
+
+        self._window.check_in_window()
+
+        # SET MODAL
+        context.window_manager.modal_handler_add(self)
+        return {'RUNNING_MODAL'}
 
 
 def register():
