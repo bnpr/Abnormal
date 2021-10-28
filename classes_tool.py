8587c150c02dd8a7271999b208af891a62db72a0
Cody Winchester <codywinchart@gmail.com>
2021-10-27 19:21:19 -0500

    Update classes_tool.py
    Add updated arguments for tool class

--- classes_tool.py
+++ classes_tool.py
@@ -7,6 +7,7 @@ class GEN_Modal_Container:
         self.pass_through_events = []
         self.cancel_keys = []
         self.confirm_keys = []
+        self.end_tool_function = None
         return
 
     #
@@ -19,6 +20,9 @@ class GEN_Modal_Container:
             tool.cancel_keys += self.cancel_keys
         if inherit_confirm:
             tool.confirm_keys += self.confirm_keys
+
+        if self.end_tool_function is not None:
+            tool.set_end_tool_function(self.end_tool_function)
 
         self.tools.append(tool)
         return tool
@@ -63,6 +67,10 @@ class GEN_Modal_Container:
         self.pass_through_events = events
         return
 
+    def set_end_tool_function(self, func):
+        self.end_tool_function = func
+        return
+
     #
 
     def __str__(self):
@@ -71,22 +79,27 @@ class GEN_Modal_Container:
 
 class GEN_Modal_Tool:
     def __init__(self):
+        self.start_arguments = []
         self.arguments = []
         self.pass_through_events = []
         self.cancel_keys = []
         self.confirm_keys = []
 
-        self.use_start = True
+        self.use_start = False
         self.initialized = False
         self.passing_through = False
         self.mouse_pass_through = False
 
-        self.start_argument = None
+        self.timer_function = None
+        self.always_function = None
         self.mouse_function = None
         self.cancel_function = None
         self.confirm_function = None
         self.pre_pass_through_function = None
         self.post_pass_through_function = None
+        self.end_tool_function = None
+        self.end_tool_confirm = True
+        self.end_tool_cancel = True
         return
 
     def test_mode(self, modal, context, event, keymap, pass_in_data):
@@ -94,17 +107,25 @@ class GEN_Modal_Tool:
 
         if self.passing_through:
             self.passing_through = False
-            if self.post_pass_through_function != None:
+            if self.post_pass_through_function is not None:
                 self.post_pass_through_function(
                     modal, context, event, pass_in_data)
 
         is_started = True
         # Check start function
-        if self.use_start and self.start_argument != None and self.initialized == False:
+        if self.use_start and self.initialized == False:
             is_started = False
 
+        # Call timer function
+        if self.always_function is not None:
+            self.always_function(modal, context, event, pass_in_data)
+
+        # Call timer function
+        if self.timer_function is not None:
+            self.timer_function(modal, context, event, pass_in_data)
+
         # Call mouse move function
-        if self.mouse_function != None and is_started:
+        if self.mouse_function is not None and is_started:
             if event.type == 'MOUSEMOVE':
                 if self.mouse_pass_through:
                     status = {"PASS_THROUGH"}
@@ -116,25 +137,41 @@ class GEN_Modal_Tool:
 
         if len(keys) > 0:
             # Check for cancelation key
-            if self.cancel_function != None:
+            if self.cancel_function is not None or self.end_tool_function is not None:
                 for c_key in self.cancel_keys:
                     if c_key in keys:
-                        # Call cancel function
-                        self.cancel_function(
-                            modal, context, event, keys, pass_in_data)
+                        if self.cancel_function is not None:
+                            # Call cancel function
+                            canc_status = self.cancel_function(
+                                modal, context, event, keys, pass_in_data)
+                            if canc_status is not None:
+                                status = canc_status
+
                         if self.use_start:
                             self.initialized = False
+
+                        if self.end_tool_function is not None and self.end_tool_cancel:
+                            self.end_tool_function(
+                                modal, context, event, keys, pass_in_data)
                         return status
 
             # Check for confirm key
-            if self.confirm_function != None and is_started:
+            if (self.confirm_function is not None or self.end_tool_function is not None) and is_started:
                 for c_key in self.confirm_keys:
                     if c_key in keys:
-                        # Call confirm function
-                        self.confirm_function(
-                            modal, context, event, keys, pass_in_data)
+                        if self.confirm_function is not None:
+                            # Call confirm function
+                            conf_status = self.confirm_function(
+                                modal, context, event, keys, pass_in_data)
+                            if conf_status is not None:
+                                status = conf_status
+
                         if self.use_start:
                             self.initialized = False
+
+                        if self.end_tool_function is not None and self.end_tool_confirm:
+                            self.end_tool_function(
+                                modal, context, event, keys, pass_in_data)
                         return status
 
             # Check for argument key
@@ -142,24 +179,29 @@ class GEN_Modal_Tool:
                 if (is_started == False and argument.pre_start) or (is_started and argument.pre_start == False):
                     if argument.key in keys:
                         # Call argument function
-                        argument.function(modal, context, event,
-                                          keys, pass_in_data)
+                        arg_status = argument.function(modal, context, event,
+                                                       keys, pass_in_data)
+                        if arg_status is not None:
+                            status = arg_status
                         return status
 
             if is_started == False:
-                if self.start_argument.key in keys:
-                    # Call argument function
-                    self.start_argument.function(
-                        modal, context, event, keys, pass_in_data)
-                    self.initialized = True
-                    return status
+                for start_argument in self.start_arguments:
+                    if start_argument.key in keys:
+                        # Call argument function
+                        arg_status = start_argument.function(
+                            modal, context, event, keys, pass_in_data)
+                        self.initialized = True
+                        if arg_status is not None:
+                            status = arg_status
+                        return status
 
         # Check for a passthrough event type
-        nav_status = test_navigation_key(self.pass_through_events, event)
-        if nav_status:
+        pass_keys = keys_find(self.pass_through_events, event)
+        if len(pass_keys) > 0:
             status = {'PASS_THROUGH'}
             self.passing_through = True
-            if self.pre_pass_through_function != None:
+            if self.pre_pass_through_function is not None:
                 self.pre_pass_through_function(
                     modal, context, event, pass_in_data)
 
@@ -190,6 +232,14 @@ class GEN_Modal_Tool:
         self.mouse_pass_through = status
         return
 
+    def set_end_tool_cancel(self, status):
+        self.end_tool_cancel = status
+        return
+
+    def set_end_tool_confirm(self, status):
+        self.end_tool_confirm = status
+        return
+
     def set_use_start(self, status):
         self.use_start = status
         return
@@ -210,8 +260,20 @@ class GEN_Modal_Tool:
         self.post_pass_through_function = func
         return
 
+    def set_end_tool_function(self, func):
+        self.end_tool_function = func
+        return
+
     def set_mouse_function(self, func):
         self.mouse_function = func
+        return
+
+    def set_timer_function(self, func):
+        self.timer_function = func
+        return
+
+    def set_always_function(self, func):
+        self.always_function = func
         return
 
     #
@@ -231,7 +293,7 @@ class GEN_Modal_Tool:
 
     def add_start_argument(self, key, func):
         arg = GEN_Argument_Function(key, func)
-        self.start_argument = arg
+        self.start_arguments.append(arg)
         return arg
 
     #
